14d6bbcf834f8cd32476f909907a963aec313008
Pedro Miguel <166464052+PedroMiguel01@users.noreply.github.com>
2026-05-28 19:56:22 +0000

    atualizando a pagina ´index.js´

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 function Home() {
-    return <h1>Seja bem vindo a um novo site!</h1>
-
-
+    return (
+        <div>
+            <h1>Seja bem vindo a um novo site!</h1>
+            <p>Esse irá ser um futuro paragrafo</p>
+        </div>
+    );
 }
 
 export default Home;
